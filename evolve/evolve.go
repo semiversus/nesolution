55578c36874dda38a00be846625d2a73264165ef
Günther Jena <guenther@jena.at>
2016-11-06 23:29:08 +0100

    running evolve parallel

--- evolve/evolve.go
+++ evolve/evolve.go
@@ -17,33 +17,46 @@ const (
   GoodEnd
 )
 
+type IterateResult struct {
+  replay *replay.Replay
+  score uint64
+}
+
 func Run(rom_path string, replay_path string) {
-  var best_score, actual_score uint64
-  var best_replay, actual_replay *replay.Replay
-  var state int
+  var best_score uint64
+  var best_replay *replay.Replay
+
+  ch := make(chan IterateResult)
 
   rand.Seed( time.Now().UnixNano())
   best_replay=replay.Load(replay_path)
 
+  _, best_score=ScoreReplay(rom_path, best_replay)
+
   for {
-    actual_replay, state, actual_score=Iterate(rom_path, best_replay)
-    fmt.Println(best_score, actual_score, state)
-    if actual_score>=best_score {
-      best_replay=actual_replay
-      best_score=actual_score
-      best_replay.Save("best.mov")
+    for i:=0; i<10; i++ {
+      go func(i int) {
+        fmt.Println("Start ", i)
+        actual_replay, _, actual_score:=Iterate(rom_path, best_replay)
+        result:=IterateResult{actual_replay, actual_score}
+        ch <- result
+        fmt.Println("Finish ", i, actual_score)
+      }(i)
     }
+    for i:=0; i<10; i++ {
+      actual_score_replay:= <- ch
+
+      if actual_score_replay.score>=best_score {
+        best_replay=actual_score_replay.replay
+        best_score=actual_score_replay.score
+        best_replay.Save("best.mov")
+      }
+    }
+    fmt.Println(best_score)
   }
 }
 
 func Iterate(rom_path string, replay_master *replay.Replay) (replay *replay.Replay, state int, score uint64) {
-  var frame_score uint64;
-	console, err := nes.NewConsole(rom_path)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-  console.Load(replay_master.GetConsoleState())
   replay_actual := replay_master.Copy()
 
   changes := int(math.Pow(10, rand.Float64()))
@@ -69,19 +82,33 @@ func Iterate(rom_path string, replay_master *replay.Replay) (replay *replay.Repl
     }
   }
 
-  for frame:=0; frame<replay_actual.Len(); frame++ {
+  state, score=ScoreReplay(rom_path, replay_actual)
+  return replay_actual, state, score
+}
+
+func ScoreReplay(rom_path string, replay *replay.Replay) (state int, score uint64) {
+  var frame_score uint64;
+
+	console, err := nes.NewConsole(rom_path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+  console.Load(replay.GetConsoleState())
+
+  for frame:=0; frame<replay.Len(); frame++ {
     console.StepFrame()
-    console.SetButtons1(replay_actual.ReadButtons(frame))
-    state, frame_score=GetScore(console)
+    console.SetButtons1(replay.ReadButtons(frame))
+    state, frame_score=GetFrameScore(console)
     score+=frame_score
     if state!=Running {
       break;
     }
   }
-  return replay_actual, state, score
+  return state, score
 }
 
-func GetScore(console *nes.Console) (state int, score uint64) {
+func GetFrameScore(console *nes.Console) (state int, score uint64) {
   score=uint64(console.RAM[0x6D])*256+uint64(console.RAM[0x86]) // x pos
   state=Running
   if console.RAM[0x0E]==0x06 || console.RAM[0x0E]==0x0B || console.RAM[0xB5]==255 {
